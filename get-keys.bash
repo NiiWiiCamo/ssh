3771e5224f2aadd43842bb134d557a91d591bb19
NiiWiiCamo <mail@nilsblume.de>
2020-12-03 17:16:41 +0100

    Update get-keys.bash

--- get-keys.bash
+++ get-keys.bash
@@ -3,6 +3,11 @@ cd ~
 mkdir -p .ssh
 auth=".ssh/authorized_keys"
 url="https://raw.githubusercontent.com/NiiWiiCamo/ssh/master/keys"
+tmp=".keys.tmp"
 touch $auth
-date +"%Y-%m-%d %T" > $auth
-curl -sSL $url >> $auth
+curl -sL -o $tmp $url
+if [ -f $tmp ]
+then
+date +"%Y-%m-%d %T" > $auth && cat $tmp >> $auth
+fi
+rm $tmp
